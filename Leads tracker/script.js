e55e702ca7911e76e6deb632724068aa91fb1d6b
Favour2333 <111124023+Favour2333@users.noreply.github.com>
2023-10-09 04:44:46 -0700

    Update script.js

--- Leads tracker/script.js
+++ Leads tracker/script.js
@@ -1,22 +1,41 @@
-const myLeads = []
+// chrome extension //
+
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const Leadsfromlocalstorage = JSON.parse( localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  renderLeads()
+
+if ( Leadsfromlocalstorage){
+    myLeads = Leadsfromlocalstorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function() {
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function () {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify( myLeads ))
+    render(myLeads)
+   console.log(tabs[0].url)
+   
+  });
+
+  
 })
 
-function renderLeads() {
+
+function render(Leads) {
   let listItems = ""
 
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < Leads.length; i++) {
     listItems += `
               <li>
-               <a target='_blank' href='${myLeads[i]}'>
-                 ${myLeads[i]}
+               <a target='_blank' href='${Leads[i]}'>
+                 ${Leads[i]}
               </a>
             </li>
                `
@@ -24,3 +43,23 @@ function renderLeads() {
 
   ulEl.innerHTML += listItems
 }
+
+deleteBtn.addEventListener('dblclick', function(){
+  console.log("doubleclicked")
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+});
+
+
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+  render(myLeads)
+
+  console.log(localStorage.getItem("myLeads"))
+})
